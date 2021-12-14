c0e8d9c6b8443064b46ec0ae9e8e335d56a5c752
Sivakiran Kakileti <sivakiran_kakileti@epam.com>
2021-12-14 21:19:52 +0530

    second commit

--- src/main/java/com/demo/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/demo/jenkins/JenkinsDemoApplication.java
@@ -9,6 +9,7 @@ public class JenkinsDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 		System.out.println("jenkins demo");
+		System.out.println("jenkins demo2");
 	}
 
 }
